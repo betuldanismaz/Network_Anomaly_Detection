1f1e535ccd2ec3adf06546e54ac4b46ab22c4308
betuldanismaz <danismazbetul3@gmail.com>
2025-11-29 16:43:36 +0300

    initial commmit

---
+++ src/capture/sniffer.py
@@ -0,0 +1,13 @@
+# src/capture/sniffer.py
+from scapy.all import sniff, IP
+
+def packet_callback(packet):
+    if packet.haslayer(IP):
+        src = packet[IP].src
+        dst = packet[IP].dst
+        print(f"Paket Yakalandı: {src} --> {dst} | Uzunluk: {len(packet)} byte")
+
+if __name__ == "__main__":
+    print("Ağ dinleniyor... (Durdurmak için Ctrl+C)")
+    # 10 paket yakalayıp duracak şekilde ayarlı
+    sniff(count=10, prn=packet_callback)
